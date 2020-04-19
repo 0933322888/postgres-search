d4f721a29818042e369d80db7b4a01d51cb7fb2c
Andrii Vasylenko <avasylenko@intellias.com>
2020-04-19 21:41:19 +0300

    final version

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# 1. npm i (this will install all needed dependencies)
-# 2. npm run serve (to start server)
-# 3. npm run start (to start client)
+Prerequisits:
+
+# 0. Have PostgreSQL installed with some database
+
+# 1. Go to /server/que.js file and adjust database params in it (user, host, database name, password etc.)
+
+# 2. run in terninal the following commands:
+
+# 2.1. npm i (this will install all needed dependencies)
+
+# 2.2. npm run serve (to start server)
+
+# 2.3. npm run start (to start http server)
 
 App will appear on localhost:3000
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ export class Home extends React.Component {
       resp: [],
       q: "",
       dic: null,
-      timeD: 0
+      timeD: 0,
     };
     this.getData = this.getData.bind(this);
     this.inputChange = this.inputChange.bind(this);
@@ -16,19 +16,19 @@ export class Home extends React.Component {
 
   inputChange(e) {
     this.setState({
-      q: e.target.value
+      q: e.target.value,
     });
   }
 
-  sendContentToDB = data => {
+  sendContentToDB = (data) => {
     // data = data[0].substr(0, 30);
     Axios.post("http://localhost:5000/send", {
-      data: data
+      data: data,
     }).then(
-      res => {
+      (res) => {
         console.log(res);
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
@@ -37,15 +37,15 @@ export class Home extends React.Component {
   getData() {
     Axios.get("http://localhost:5000/search", {
       params: {
-        q: this.state.q
-      }
+        q: this.state.q,
+      },
     }).then(
-      res => {
+      (res) => {
         this.setState({
-          resp: res?.data
+          resp: res?.data,
         });
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
@@ -55,7 +55,7 @@ export class Home extends React.Component {
     let input = ev.target.files;
     let promises = [];
     for (let i = 0; i < input.length; i++) {
-      let promise = new Promise(resolve => {
+      let promise = new Promise((resolve) => {
         const file = input[i];
         const reader = new FileReader();
         reader.readAsText(file);
@@ -64,7 +64,7 @@ export class Home extends React.Component {
       promises.push(promise);
     }
 
-    Promise.all(promises).then(content => {
+    Promise.all(promises).then((content) => {
       const startTime = Date.now();
       //   let linesArrs = this.splitByLines(content);
       this.sendContentToDB(content);
@@ -83,11 +83,11 @@ export class Home extends React.Component {
     });
   }
 
-  splitByLines = content => {
+  splitByLines = (content) => {
     let list = [];
 
     for (let i = 0; i < content.length; i++) {
-      let splittedByLine = content[i].split(/\r?\n/).filter(el => el !== "");
+      let splittedByLine = content[i].split(/\r?\n/).filter((el) => el !== "");
       list.push({ file: i, lines: splittedByLine });
     }
 
@@ -97,13 +97,13 @@ export class Home extends React.Component {
   showOnHTML = (dictionary, timeDelta) => {
     this.setState({
       dic: dictionary.length,
-      timeD: timeDelta
+      timeD: timeDelta,
     });
   };
 
-  saveToFile = dictionary => {
+  saveToFile = (dictionary) => {
     var file = new Blob([JSON.stringify(dictionary)], {
-      type: "text/plain;charset=utf-8"
+      type: "text/plain;charset=utf-8",
     });
     var a = document.createElement("a"),
       url = URL.createObjectURL(file);
@@ -111,13 +111,13 @@ export class Home extends React.Component {
     a.download = "dictionary.txt";
     document.body.appendChild(a);
     a.click();
-    setTimeout(function() {
+    setTimeout(function () {
       document.body.removeChild(a);
       window.URL.revokeObjectURL(url);
     }, 0);
   };
 
-  generateListOfAllWords = content => {
+  generateListOfAllWords = (content) => {
     let list = [];
 
     for (let i = 0; i < content.length; i++) {
@@ -146,17 +146,17 @@ export class Home extends React.Component {
     const words = content[i].split(
       /\s|\r|,|\.|—|-|\/|\(|\)|:|;|\[|\]|"|'|\?|!|&|\d/
     );
-    const filtered = words.filter(el => el !== ""); // remove spaces
+    const filtered = words.filter((el) => el !== ""); // remove spaces
     const sorted = filtered.sort(); //sort //O(nLogn)
-    sorted.forEach(word => {
+    sorted.forEach((word) => {
       preparationArr.push({ docId: i + 1, term: word, frequency: 1 });
     });
     return preparationArr;
   };
 
-  sortByWords = list => {
+  sortByWords = (list) => {
     //O(nLogn)
-    return list.sort(function(a, b) {
+    return list.sort(function (a, b) {
       if (a.term > b.term) {
         return 1;
       }
@@ -167,24 +167,24 @@ export class Home extends React.Component {
     });
   };
 
-  generateDictionary = sortedList => {
+  generateDictionary = (sortedList) => {
     let dict = [];
     let firstPopped = sortedList.pop();
     dict.push({
       docIds: [{ docId: firstPopped.docId, frequency: firstPopped.frequency }],
-      term: firstPopped.term
+      term: firstPopped.term,
     });
     while (sortedList.length) {
       let popped = sortedList.pop();
       if (dict[dict.length - 1].term !== popped.term) {
         dict.push({
           term: popped.term,
-          docIds: [{ docId: popped.docId, frequency: popped.frequency }]
+          docIds: [{ docId: popped.docId, frequency: popped.frequency }],
         });
       } else {
         dict[dict.length - 1].docIds.push({
           docId: popped.docId,
-          frequency: popped.frequency
+          frequency: popped.frequency,
         });
         dict[dict.length - 1].docIds.sort((a, b) => {
           return a.docId - b.docId;
@@ -199,8 +199,20 @@ export class Home extends React.Component {
       <>
         <div
           style={{
+            textAlign: "center",
+          }}
+        >
+          <div>Instructions:</div>
+          <div>1. Upload some files via input at the bottom of the page</div>
+          <div>2. Enter some text in Google-like form and press Search</div>
+          <div>
+            Result: You will see search results (if any) right below the button
+          </div>
+        </div>
+        <div
+          style={{
             height: "100vh",
-            textAlign: "center"
+            textAlign: "center",
           }}
         >
           <img
@@ -222,7 +234,7 @@ export class Home extends React.Component {
                 margin: 10,
                 boxShadow: "1px 1px #dfdfdf",
                 outline: "none",
-                padding: "0 20px"
+                padding: "0 20px",
               }}
             />
           </div>
@@ -233,11 +245,11 @@ export class Home extends React.Component {
                 width: 130,
                 height: 33,
                 borderRadius: 6,
-                color: "gray"
+                color: "gray",
               }}
               onClick={this.getData}
             >
-              Search in Google
+              Search
             </button>
           </div>
 
